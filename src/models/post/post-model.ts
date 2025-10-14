bd9d99574d1a3138469d4f5b813f7cadd92e7653
Wagner Kauan <155324620+WagnerKauan@users.noreply.github.com>
2025-10-13 21:17:41 -0300

    feat: Criando a tipagem dos posts e padrão de projeto repository

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,13 @@
+export type PostModel = {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  coverImageUrl: string;
+  published: string;
+  createdAt: string;
+  updatedAt: string;
+  author: string;
+};
+
